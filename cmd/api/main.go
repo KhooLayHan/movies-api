b5a3b25c513bdd3badf579c5c4ac6395e3d6d016
KhooLayHan <khoolayhan@github.com>
2025-10-07 10:54:51 +0800

    feat: Updated to use chi router instead.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	// Initialize the chi router.
+	router := chi.NewRouter()
 
 	// Register the healthcheck handler for the "/v1/healthcheck" route.
-	mux.HandleFunc("/v1/healthcheck", healthCheckHandler)
+	router.Get("/v1/healthcheck", healthCheckHandler)
 
 	// Server port to listen on.
 	port := 4040
@@ -18,8 +21,14 @@ func main() {
 	// Print a message indicating the server is starting
 	log.Printf("Starting server on port %d", port)
 
+	// Creates the HTTP server with chi router
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: router,
+	}
+
 	// Start the HTTP server with our server mux.
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
