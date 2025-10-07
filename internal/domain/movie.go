2c332db128e204d8f622ef0534e26e63b2a78541
KhooLayHan <khoolayhan@github.com>
2025-10-07 17:42:21 +0800

    feat: Added Movie struct for future database schema.

---
+++ internal/domain/movie.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"time"
+)
+
+// Movie represents a single movie in our database.
+type Movie struct {
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int       `json:"version,omitempty"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
