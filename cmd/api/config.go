43f7334d0d9246dba36d202c38066f734cb87f41
KhooLayHan <khoolayhan@github.com>
2025-10-08 02:36:32 +0800

    refactor: Updated to use .env config variables instead with LoadConfig() function.

---
+++ cmd/api/config.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+// Config represents the configuration for the application.
+type Config struct {
+	Port int
+	DB   struct {
+		DSN string
+	}
+}
+
+// LoadConfig loads the application configuration from environment variables.
+func LoadConfig() (Config, error) {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var cfg Config
+
+	// Parse the server port
+	portStr := os.Getenv("API_PORT")
+	if portStr == "" {
+		log.Fatal("API_PORT environment variable not set")
+	}
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatal("Invalid API_PORT environment variable")
+		return Config{}, err
+	}
+	cfg.Port = port
+
+	// Parse the database DSN
+	dbConnection := os.Getenv("DB_CONNECTION")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	cfg.DB.DSN = fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable", dbConnection, dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	fmt.Println(cfg.DB.DSN)
+
+	if cfg.DB.DSN == "" {
+		log.Fatal("DB_DSN environment variable not set")
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // application struct to hold application-wide state
 type application struct {
-	// Any dependencies required by the application
+	config Config
+	db     *pgxpool.Pool
 }
 
 func main() {
-	// Initialize the application struct
-	app := &application{}
+	// Load the configuration.
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Connect to the database.
+	db, err := openDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Defers the database call so that the connection pool closes before exiting the main() function.
+	defer db.Close()
+	log.Println("Database connection pool established.")
+
+	// Initialize the application struct.
+	app := &application{
+		config: cfg,
+		db:     db,
+	}
 
 	// Initialize the chi router.
 	router := chi.NewRouter()
@@ -38,8 +61,27 @@ func main() {
 	}
 
 	// Start the HTTP server with our server mux.
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// openDB creates a new connection pool for a given configuration.
+func openDB(cfg Config) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(context.Background(), cfg.DB.DSN)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	// Creates a context with a 5-second timeout to test the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Ping the database to test the connection
+	if err := db.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return db, nil
 }
