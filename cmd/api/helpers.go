a7834a981e02f8b4369e85837c49cc10b9d3be2b
KhooLayHan <khoolayhan@github.com>
2025-10-07 17:43:28 +0800

    feat: Added writeJSON helper func for sending JSON responses.

---
+++ cmd/api/helpers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// envelope is a helper type for unwrapping JSON responses.
+type envelope map[string]any
+
+// writeJSON is a helper function for sending JSON responses.
+// It takes the destination http.ResponseWriter, the HTTP status code, the data to encode,
+// and a header map.
+func writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if _, err := w.Write(js); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
+}
